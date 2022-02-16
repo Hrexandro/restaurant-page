083c1e4e05536c0adaf276bf60d23a6a39c7189e
Hrexandro <marcin_nowak_hrx@o2.pl>
2022-02-16 07:15:52 +0100

    tinker with styling

--- src/home.js
+++ src/home.js
@@ -3,8 +3,8 @@ const home = () => {
     const descriptionContainer = document.createElement('div');
     const description = document.createElement('p');
     description.setAttribute('id','description')
-    description.textContent = 'Traditional Polish dishes prepared with love and passion using only local & natual ingredience.Traditional Polish dishes prepared with love and passion using only local & natual ingredience.Traditional Polish dishes prepared with love and passion using only local & natual ingredience.Traditional Polish dishes prepared with love and passion using only local & natual ingredience.Traditional Polish dishes prepared with love and passion using only local & natual ingredience.';
-
+    description.textContent = 'Traditional Polish dishes prepared with love and passion using only local & natual ingredience. The ingerdients are local and natural, and the dishes prepared with love and tradition. Traditional dishes, just like our grandmas used to make. Traditional Polish dishes prepared with love and passion using only local & natual ingredience. Be sure to try our soup of the day.';
+    description.setAttribute('id', 'description')
 
 
     document.body.appendChild(descriptionContainer);
