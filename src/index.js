e31cff0e30e77f7d078194b1d1796cb51ff096a8
Hrexandro <marcin_nowak_hrx@o2.pl>
2022-02-17 21:24:50 +0100

    fix font and style loading

--- src/index.js
+++ src/index.js
@@ -1,56 +1,60 @@
-import loadHomepage from './home.js'
+import './style.css';
 
-// TO DO:
-// SET THE OTHER PAGES WITH CONTENT
-// ADD PAGE SWITCHING LOGIC TO index.JS
 
-//font used:
-// https://fonts.google.com/specimen/Gwendolyn?preview.text=Symphonie&preview.text_type=custom&category=Handwriting
-//maybe remove bold, if not used after all
+const loadHomepage = () => {
+    console.log('home works')
+
+    let subtitle = document.createElement('p');
+    document.getElementById('main').appendChild(subtitle);
+    subtitle.textContent = "Restaurant"
+
+    let title = document.createElement('p');
+    document.getElementById('main').prepend(title);
+    title.textContent = "Symphonie"
+    title.setAttribute('class', 'cursive')
+
+    console.log('home finish')
+}
+
 let content = document.createElement('div');
-content.setAttribute('id','content');
+console.log(content)
+content.setAttribute('id', 'content');
+console.log(document.body)
 document.body.appendChild(content);
 
 
 let header = document.createElement('header');
 let navbar = document.createElement('nav');
-
-let styleSheet = document.createElement('link');
-styleSheet.href = 'style.css';
-styleSheet.rel = 'stylesheet';
-document.head.appendChild(styleSheet);
+//dodaj to jak już będzie stylesheet
+// let styleSheet = document.createElement('link');
+// styleSheet.href = 'style.css';
+// styleSheet.rel = 'stylesheet';
+// document.head.appendChild(styleSheet);
 
 content.appendChild(header);
 header.appendChild(navbar);
 
 
-function addLinkToNavbar (name, reference){
+function addLinkToNavbar(name, reference) {
     let link = document.createElement('a')
     link.href = reference;
     link.textContent = name;
     navbar.appendChild(link);
 }
 
-addLinkToNavbar('Home','#')
-addLinkToNavbar('Menu','#')
-addLinkToNavbar('Contact','#')
+addLinkToNavbar('Home', '#')
+addLinkToNavbar('Menu', '#')
+addLinkToNavbar('Contact', '#')
 
 let main = document.createElement('main');
 content.appendChild(main);
-main.setAttribute('id','main')
+main.setAttribute('id', 'main')
 
 let footer = document.createElement('footer');
 content.appendChild(footer)
 footer.textContent = 'Made by Hrexandro'
 
-// let subtitle = document.createElement('h2');
-// header.prepend(subtitle);
-// subtitle.textContent = "Restaurant"
 
-// let title = document.createElement('h1');
-// header.prepend(title);
-// title.textContent = "Symphonie"
 
 
 loadHomepage();
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+
+};
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-    home: './src/home.js',
-    menu: './src/menu.js',
-    contact: './src/contact.js',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
+  devtool: "inline-source-map",
+
   module: {
     rules: [
       {
@@ -16,23 +21,11 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf|truetype)$/i,
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Symphonie Restaurant',
-    }),
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    
   },
 
 
